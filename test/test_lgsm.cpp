1bf9b51ab1a487d6e396e12bc3287eb5e8fd42f7
Antoine Hoarau <hoarau@isir.upmc.fr>
2016-09-04 22:37:26 +0200

    add use case to show eigen3.3 bug #2

--- test/test_lgsm.cpp
+++ test/test_lgsm.cpp
@@ -1,7 +1,23 @@
-#include <Eigen/Dense>
 #include <Eigen/Lgsm>
+#include <iostream>
 
 int main(int argc, char ** argv)
 {
-	return 0;
+    // Eigen::Displacementd a,b;
+    Eigen::Twistd c;
+    Eigen::Wrenchd d;
+    //
+    //Eigen::Vector3d test = d.getTorque();
+    std::cout << c.getAngularVelocity() << std::endl;
+    // // std::cout << d.getTorque() << std::endl;
+    // // std::cout << d.getForce() << std::endl;
+    // //
+    // // std::cout << a.adjoint() << std::endl;
+    // auto e = a*b;
+    //
+    // std::cout << "a : " << a << std::endl;
+    // std::cout << "b : " << b << std::endl;
+    // std::cout << "a*b : " << e << std::endl;
+
+    return 0;
 }
